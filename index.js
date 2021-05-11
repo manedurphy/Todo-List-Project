c0b96ac6b1576b823f939bcf1defa26504b9cb3b
Dane Murphy <danem7@gmail.com>
2021-05-10 22:02:52 -0700

    added a readme and dockerized the application

--- index.js
+++ index.js
@@ -1,47 +1,9 @@
 const express = require('express');
 const path = require('path');
-const reload = require('reload');
-const http = require('http');
-
 const port = 3000;
 const app = express();
 const publicDir = path.join(__dirname, 'public');
 
-/*
-
-FOR THIS TO WORK: (You only have to do this once)
-
-run terminal command:
-> npm init (when in doubt, just hit enter)
-> npm i -D nodemon reload
-> npm i express
-
-paste: "start": "nodemon --watch public -e js,html,css",
-into "scripts" field in package.json
-
-then, in your index.html file (right above the </body> closing tag) paste:
-<script src="/reload/reload.js"></script>
-
-- make sure you put all of your other html/js/css files/folders into the public/ folder
-
-TO RUN THE SERVER:
-> npm run start
-*/
-
-
 app.use(express.static(path.join(publicDir)));
-app.set('port', process.env.PORT || port);
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(publicDir, "index.html"));
-})
-
-var server = http.createServer(app);
-
-reload(app).then(function(reloadReturned) {
-  server.listen(app.get('port'), () => {
-    console.log("Listening on http://localhost:" + app.get('port'));
-  })
-}).catch(function(err) {
-  console.error('Reload could not start, could not start server/sample app', err)
-})
+app.listen(port, () => console.log('Server started on port 3000'));
--- public/app.js
+++ public/app.js
@@ -1,116 +1,96 @@
-// const { domainToUnicode } = require("url");
-
-// const e = require("express");
-
 //Selectors
-const todoInput = document.querySelector(".todo-input");
-const todoButton = document.querySelector(".todo-button");
-const todoList = document.querySelector(".todo-list");
-const filterOption = document.querySelector(".filter-todo");
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo');
 
 //Event listeners
-todoButton.addEventListener("click", addTodo); //we are adding an event to the todo-button that we created in our html code. this event is creating a div that we later append into the  todo-container
-todoList.addEventListener("click", deleteCheck);
-filterOption.addEventListener("click", filterTodo);
+todoButton.addEventListener('click', addTodo); // We are adding an event to the todo-button that we created in our html code. this event is creating a div that we later append into the  todo-container
+todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo);
 
 //Functions
 function addTodo(event) {
-  //this entire addOne function is going to be appplied only to todoButton
-  //Prevent from submitting
-  event.preventDefault();
-  //Create a toDo div
-  const todoDiv = document.createElement("div"); //creating the div
-  todoDiv.classList.add("one-todo-item"); //giving todoDiv a class label that we can reference in CSS when styling
+    // This entire addOne function is going to be appplied only to todoButton
+    // Prevent from submitting
+    event.preventDefault();
+    //Create a toDo div
+    const todoDiv = document.createElement('div'); //creating the div
+    todoDiv.classList.add('one-todo-item'); //giving todoDiv a class label that we can reference in CSS when styling
 
-  //Create LI
-  const newTodo = document.createElement("li");
-  newTodo.innerText = todoInput.value; //making sure that whatever is submitting into the form input is displayed in that specific todo list item
-  //rememeber: todoInput is has the todoButton declared inside of it in the html
-  newTodo.classList.add("todo-item");
-  todoDiv.appendChild(newTodo); //sticking new todo inside div we just created. we will be appending todoDiv to the todo-list class unordered list at the end of this function
+    //Create LI
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value; // Making sure that whatever is submitting into the form input is displayed in that specific todo list item
 
-  //Now we need complete and delete buttons
-  //Check button
-  const completedButton = document.createElement("button");
-  completedButton.innerHTML = '<i class="fas fa-check"></i>'; //adding this 'i-tag' using innerHTML function
-  completedButton.classList.add("complete-btn");
-  todoDiv.appendChild(completedButton);
-  // delete button
-  const trashButton = document.createElement("button");
-  trashButton.innerHTML = '<i class="fas fa-trash"></i>'; //adding this 'i-tag' using innerHTML function
-  trashButton.classList.add("trash-btn");
-  todoDiv.appendChild(trashButton);
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo); //sticking new todo inside div we just created. we will be appending todoDiv to the todo-list class unordered list at the end of this function
 
-  //APPEND TO LIST
-  todoList.appendChild(todoDiv);
+    // Now we need complete and delete buttons
+    // Check button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>'; //adding this 'i-tag' using innerHTML function
+    completedButton.classList.add('complete-btn');
+    todoDiv.appendChild(completedButton);
+    // Delete button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>'; //adding this 'i-tag' using innerHTML function
+    trashButton.classList.add('trash-btn');
+    todoDiv.appendChild(trashButton);
 
-  //CLEAR INPUT TODO VALUE
-  todoInput.value = "";
+    //APPEND TO LIST
+    todoList.appendChild(todoDiv);
+
+    //CLEAR INPUT TODO VALUE
+    todoInput.value = '';
 }
 //CREATE DELETE-CHECK FUNCTION
 function deleteCheck(event) {
-  // console.log("ran delete check");
+    const item = event.target;
+    console.log(item.parentElement);
 
-  const item = event.target;
-  console.log(item.parentElement);
-  // console.log(item);
-  //DELETE TODO
-  if (item.classList[0] === "trash-btn") {
-    const todo = item.parentElement; //this is how we delete the todo list item of our choice without deleting the input line
-    // console.log(todo);
-    //Animation
-    todo.classList.add("fall");
-    todo.addEventListener("transitionend", function () {
-      todo.remove(); //this means the item will be deleted only after the transition has been completed
-    });
-    // todo.remove();
-  }
+    //DELETE TODO
+    if (item.classList[0] === 'trash-btn') {
+        const todo = item.parentElement; //this is how we delete the todo list item of our choice without deleting the input line
 
-  //CHECK MARK
-  if (item.classList[0] === "complete-btn") {
-    const todo = item.parentElement;
-    todo.classList.toggle("completed");
-  }
-}
-
-//Now we need to address the filter from <select></select>
-function filterTodo(event) {
-  const todos = todoList.childNodes;
-  todos.forEach(function (todo) {
-    switch (
-      event.target.value //the value can be 'complete', 'uncompleted', or 'all'
-    ) {
-      case "all":
-        todo.style.display = "flex";
-        break;
-      case "completed":
-        todo.style.display = "flex";
-        if (todo.classList.contains("completed")) {
-          todo.style.display = "flex";
-        } else {
-          todo.style.display = "none";
-        }
-        break;
-      case "uncompleted":
-        if (!todo.classList.contains("completed")) {
-          todo.style.display = "flex";
-        } else {
-          todo.style.display = "none";
-        }
-        break;
+        //Animation
+        todo.classList.add('fall');
+        todo.addEventListener('transitionend', function () {
+            todo.remove(); //this means the item will be deleted only after the transition has been completed
+        });
     }
-  });
-  // console.log(todos);
+
+    //CHECK MARK
+    if (item.classList[0] === 'complete-btn') {
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
+    }
 }
 
-// function saveLocalTodos(todo) {
-//   let todos;
-
-//   if (localStorage.getItem("todos") === null) {
-//     todos = [];
-//   } else {
-//     todos = JSON.parse(localStorage.getItem("todos"));
-//   }
-//   todos.push(todo);
-//   localStorage.setItem("todos", JSON.stringify(todos));
-// }
+// Now we need to address the filter from <select></select>
+function filterTodo(event) {
+    const todos = todoList.childNodes;
+    todos.forEach(function (todo) {
+        switch (
+            event.target.value //the value can be 'complete', 'incomplete', or 'all'
+        ) {
+            case 'all':
+                todo.style.display = 'flex';
+                break;
+            case 'completed':
+                todo.style.display = 'flex';
+                if (todo.classList.contains('completed')) {
+                    todo.style.display = 'flex';
+                } else {
+                    todo.style.display = 'none';
+                }
+                break;
+            case 'incomplete':
+                if (!todo.classList.contains('completed')) {
+                    todo.style.display = 'flex';
+                } else {
+                    todo.style.display = 'none';
+                }
+                break;
+        }
+    });
+}
